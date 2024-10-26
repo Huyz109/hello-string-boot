48652a2a5c16113f50a2cae738228f83058117b5
Huyz109 <phamqhuy2001@gmail.com>
2024-10-26 15:58:44 +0700

    update CRUD user API

--- src/main/java/com/spring/hello_spring_boot/controller/UserController.java
+++ src/main/java/com/spring/hello_spring_boot/controller/UserController.java
@@ -1,21 +1,43 @@
 package com.spring.hello_spring_boot.controller;
 
 import com.spring.hello_spring_boot.dto.request.UserCreationRequest;
+import com.spring.hello_spring_boot.dto.request.UserUpdateRequest;
 import com.spring.hello_spring_boot.entity.User;
 import com.spring.hello_spring_boot.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@RequestMapping("/user")
 public class UserController {
     @Autowired
     private UserService userService;
 
-    @PostMapping("/user")
+    @PostMapping()
     User createUser(@RequestBody UserCreationRequest request) {
         return userService.createUser(request);
     }
 
+    @GetMapping("/list")
+    List<User> getAllUsers(){
+        return userService.getAllUsers();
+    }
+
+    @GetMapping("/{userId}")
+    User getUserById(@PathVariable("userId") String userId){
+        return userService.getUserById(userId);
+    }
+
+    @PutMapping("/{userId}")
+    User updateUser(@PathVariable("userId") String userId, @RequestBody UserUpdateRequest request){
+        return userService.updateUser(userId, request);
+    }
+
+    @DeleteMapping("/{userId}")
+    String deleteUser(@PathVariable("userId") String userId){
+        userService.deleteUser(userId);
+        return "User has been deleted";
+    }
 }
--- src/main/java/com/spring/hello_spring_boot/service/UserService.java
+++ src/main/java/com/spring/hello_spring_boot/service/UserService.java
@@ -1,10 +1,13 @@
 package com.spring.hello_spring_boot.service;
 
 import com.spring.hello_spring_boot.dto.request.UserCreationRequest;
+import com.spring.hello_spring_boot.dto.request.UserUpdateRequest;
 import com.spring.hello_spring_boot.entity.User;
 import com.spring.hello_spring_boot.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class UserService {
@@ -21,5 +24,29 @@ public class UserService {
         user.setDob(request.getDob());
 
         return userRepository.save(user);
+    }
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public User getUserById(String id) {
+        return userRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+    }
+
+    public User updateUser(String id, UserUpdateRequest request) {
+        User user = getUserById(id);
+
+        user.setPassword(request.getFirstName());
+        user.setFirstName(request.getFirstName());
+        user.setLastName(request.getLastName());
+        user.setDob(request.getDob());
+
+        return userRepository.save(user);
+    }
+
+    public void deleteUser(String id) {
+        userRepository.deleteById(id);
     }
 }
