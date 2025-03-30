1506a3221a65de7930af8057b7e438bc2fd26f6e
Huyz109 <phamqhuy2001@gmail.com>
2025-03-30 13:14:28 +0700

    isolation unit test

--- src/test/java/com/spring/hello_spring_boot/controller/UserControllerTest.java
+++ src/test/java/com/spring/hello_spring_boot/controller/UserControllerTest.java
@@ -1,6 +1,5 @@
 package com.spring.hello_spring_boot.controller;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.spring.hello_spring_boot.dto.request.UserCreationRequest;
@@ -16,16 +15,17 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 
 @Slf4j
 @SpringBootTest
 @AutoConfigureMockMvc
+@TestPropertySource("/test.properties")
 public class UserControllerTest {
     @Autowired
     private MockMvc mockMvc;
